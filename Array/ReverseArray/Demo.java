93c119084f089a51bbf9253e61b861b386feee58
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-10-30 23:39:56 +0530

    Add files via upload

---
+++ Array/ReverseArray/Demo.java
@@ -0,0 +1,22 @@
+package GithunDSA.Array.ReverseArray;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int arr[] = {10, 30, 20, 50, 60};
+
+        int n = arr.length;
+
+        //...............................................................................//
+
+        System.out.println("Revers Array is :- ");
+        for (int i=n-1; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+
+        //...............................................................................//
+
+    }
+    
+}
