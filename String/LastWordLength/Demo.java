a8d25366c99e5efc3a23a7159b1fe58fb3026c10
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-11-09 22:01:40 +0530

    Add files via upload

---
+++ String/LastWordLength/Demo.java
@@ -0,0 +1,37 @@
+package GithunDSA.String.LastWordLength;
+
+import java.util.Scanner;
+
+public class Demo {
+
+    //............................................................................//
+    public static int lastWordlength(String s){        //function declaration
+
+        int length = 0;
+
+        for (int i=s.length()-1; i>=0; i--){
+            if (s.charAt(i) != ' '){
+                length++;
+            }
+            else {
+                break;
+            }
+        }
+        return length;
+    }
+    //............................................................................//
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter String :- ");
+
+        Scanner sc = new Scanner(System.in);
+        String ab = sc.nextLine();
+
+        int len = lastWordlength(ab);                  //function call
+
+        System.out.println("The Length of last word is :- " + len);
+
+    }
+
+}
